4dd8e2be8592a8b2149b3edc5598d72af692ef92
vadim7809 <mandyuk.vadim@gmail.com>
2025-02-16 16:31:06 +0200

    py

--- main.py
+++ main.py
@@ -6,7 +6,40 @@ window = QWidget()
 window.resize(500, 500)
 
 
+main_line = QHBoxLayout()
+
+# Тут ліва частина
+text_edit = QTextEdit()
+main_line.addWidget(text_edit)
 
 
+#Тут права частина
+v1 = QVBoxLayout()
+
+notes_list_lbl = QLabel("Список заміток")
+notes_list = QListWidget()
+
+v1.addWidget(notes_list_lbl)
+v1.addWidget(notes_list)
+
+main_line.addLayout(v1)
+
+main_btn = QHBoxLayout()
+add_note_btn = QPushButton("Створити замітку")
+delete_note_btn = QPushButton("Видалити замітку")
+save_note_btn = QPushButton("Зберегти замітку")
+main_btn.addWidget(add_note_btn)
+main_btn.addWidget(delete_note_btn)
+main_btn.addWidget(save_note_btn)
+main_line.addLayout(main_btn)
+
+notes_list_lbl2 = QLabel("Список тегів")
+tag_list = QListWidget()
+v1.addWidget(tag_list)
+v1.addWidget(notes_list_lbl2)
+
+tag_input = QLineEdit()
+
+window.setLayout(main_line)
 window.show()
 app.exec()
