274d1261d6bf33917b8548f3c364dedb650adb18
vadim7809 <mandyuk.vadim@gmail.com>
2025-02-22 17:31:17 +0200

    py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import*
 
+
 app = QApplication([])
+
 window = QWidget()
-window.resize(500, 500)
+window.resize(600, 400)
 
 
-main_line = QHBoxLayout()
+main_line = QHBoxLayout(window)
 
-# Тут ліва частина
+
 text_edit = QTextEdit()
 main_line.addWidget(text_edit)
 
 
-#Тут права частина
 v1 = QVBoxLayout()
 
-notes_list_lbl = QLabel("Список заміток")
-notes_list = QListWidget()
+# Заголовок списку нотаток
+v1.addWidget(QLabel("Список заміток"))
 
-v1.addWidget(notes_list_lbl)
-v1.addWidget(notes_list)
 
-main_line.addLayout(v1)
+note_list = QListWidget()
+v1.addWidget(note_list)
+
 
 main_btn = QHBoxLayout()
 add_note_btn = QPushButton("Створити замітку")
@@ -30,15 +31,34 @@ delete_note_btn = QPushButton("Видалити замітку")
 save_note_btn = QPushButton("Зберегти замітку")
 main_btn.addWidget(add_note_btn)
 main_btn.addWidget(delete_note_btn)
-main_btn.addWidget(save_note_btn)
-main_line.addLayout(main_btn)
+v1.addWidget(save_note_btn)
+v1.addLayout(main_btn)
 
-notes_list_lbl2 = QLabel("Список тегів")
+
+v1.addWidget(QLabel("Список тегів"))
+
+
 tag_list = QListWidget()
 v1.addWidget(tag_list)
-v1.addWidget(notes_list_lbl2)
+
 
 tag_input = QLineEdit()
+tag_input.setPlaceholderText("Введіть тег...")
+v1.addWidget(tag_input)
+
+
+tag_btn_layout = QHBoxLayout()
+add_tag_btn = QPushButton("Додати до замітки")
+remove_tag_btn = QPushButton("Відкріпити від замітки")
+tag_btn_layout.addWidget(add_tag_btn)
+tag_btn_layout.addWidget(remove_tag_btn)
+v1.addLayout(tag_btn_layout)
+
+search_btn = QPushButton("Шукати замітки по тегу")
+v1.addWidget(search_btn)
+
+main_line.addLayout(v1)
+
 
 window.setLayout(main_line)
 window.show()
